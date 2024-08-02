d7a422bd6a743e618a20d183db15d6fd710ca8ca
gmogro <guillermomogro51@gmail.com>
2024-08-02 12:05:37 -0300

    Clase 04

---
+++ Clase 04/example.py
@@ -0,0 +1,20 @@
+# Definiendo variables
+numero = 11
+linea = "Esto es un string"
+real = 12.0345
+resultado = True
+
+#Ingresando Datos
+nombre = input("Ingrese nombre: ")
+numero2 = int(input("Ingrese un numero: "))
+real2 = float(input("Ingrese un real: "))
+boleano = bool(input("Ingrese un booleano: "))
+
+#Mostramos datos
+mostrar = f"Usted ingreso el numero: {numero2}"
+print(f"Mi nombre es : {nombre}")
+print(mostrar)
+
+#Type Funcion
+tipo = type(numero)
+print(tipo)
